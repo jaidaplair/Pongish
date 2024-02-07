36a976aef9a6667427a29c1640dcc3e5fe2394a4
Jaida Plair <jaiplair@gmail.com>
2024-02-07 09:51:13 -0500

    made paddles move and stop on boundrys

--- Assets/secondPaddleScript.cs
+++ Assets/secondPaddleScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class paddleScript : MonoBehaviour
+public class secondPaddleScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,16 +13,16 @@ public class paddleScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.UpArrow))
         {
             transform.Translate(0f, 0.02f, 0f);
-            if(transform.position.y >= 2.29f)
+            if (transform.position.y >= 2.29f)
             {
                 var x = transform.position.x;
-                transform.transform.position = new Vector3(x,2.29f,0f);
+                transform.transform.position = new Vector3(x, 2.29f, 0f);
             }
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.DownArrow))
         {
             transform.Translate(0f, -0.02f, 0f);
             if (transform.position.y <= -2.4f)
@@ -32,9 +32,4 @@ public class paddleScript : MonoBehaviour
             }
         }
     }
-
-        //Debug.Log(transform.position.y);
-        
-
 }
-

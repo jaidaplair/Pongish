8ac0b8c380a42d8094c4716a8e4af0fe115da42d
Jaida Plair <jaiplair@gmail.com>
2024-02-18 21:12:02 -0500

    paddle changes

--- Assets/paddleScript.cs
+++ Assets/paddleScript.cs
@@ -4,6 +4,37 @@ using UnityEngine;
 
 public class paddleScript : MonoBehaviour
 {
+    public float speed = 5f;
+    public float maxY = 2.29f;
+    public float minY = -2.4f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move paddle up if W key is pressed
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0f, speed * Time.deltaTime, 0f);
+            // Limit paddle movement to maxY
+            if (transform.position.y >= maxY)
+            {
+                transform.position = new Vector3(transform.position.x, maxY, 0f);
+            }
+        }
+        // Move paddle down if S key is pressed
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0f, -speed * Time.deltaTime, 0f);
+            // Limit paddle movement to minY
+            if (transform.position.y <= minY)
+            {
+                transform.position = new Vector3(transform.position.x, minY, 0f);
+            }
+        }
+    }
+
+
+    /*
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +51,7 @@ public class paddleScript : MonoBehaviour
             {
                 var x = transform.position.x;
                 transform.transform.position = new Vector3(x,2.29f, 0f);
+                
             }
         }
         if (Input.GetKey(KeyCode.S))
@@ -29,12 +61,13 @@ public class paddleScript : MonoBehaviour
             {
                 var x = transform.position.x;
                 transform.transform.position = new Vector3(x, -2.4f, 0f);
+                
             }
         }
     }
 
         //Debug.Log(transform.position.y);
         
-
+*/
 }
 
--- Assets/secondPaddleScript.cs
+++ Assets/secondPaddleScript.cs
@@ -4,6 +4,36 @@ using UnityEngine;
 
 public class secondPaddleScript : MonoBehaviour
 {
+    public float speed = 5f;
+    public float maxY = 2.29f;
+    public float minY = -2.4f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move paddle up if W key is pressed
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0f, speed * Time.deltaTime, 0f);
+            // Limit paddle movement to maxY
+            if (transform.position.y >= maxY)
+            {
+                transform.position = new Vector3(transform.position.x, maxY, 0f);
+            }
+        }
+        // Move paddle down if S key is pressed
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0f, -speed * Time.deltaTime, 0f);
+            // Limit paddle movement to minY
+            if (transform.position.y <= minY)
+            {
+                transform.position = new Vector3(transform.position.x, minY, 0f);
+            }
+        }
+    }
+
+    /*
     // Start is called before the first frame update
     void Start()
     {
@@ -32,4 +62,5 @@ public class secondPaddleScript : MonoBehaviour
             }
         }
     }
+    */
 }
